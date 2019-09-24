bee38e68f3576fd1231ea52b88aeb96818944185
ralex <ralex.rdz@gmail.com>
2019-09-23 19:35:18 -0500

    with svelt-apollo

--- src/routes/familias/index.svelte
+++ src/routes/familias/index.svelte
@@ -4,20 +4,25 @@
   import FAMILIAS_MANY from '../../consultas/FAMILIAS_MANY' 
 
   export async function preload() {
-    let familias = await client.query({
-      query: FAMILIAS_MANY
-    })
+    let aux = await client.query({
+        query: FAMILIAS_MANY
+      })
     return {
-      familias: familias.data.FamiliaMany
+      cache: aux
     }
   }
 </script>
 
 <script>
-  export let familias;
+  import { restore, query } from 'svelte-apollo';
 
-  console.log(familias)
+  export let cache;
 
+  restore(client, FAMILIAS_MANY, cache.data);
+  const familias = query(client, { query: FAMILIAS_MANY });
+  function reload() {
+    familias.refetch();
+  }
 </script>
 
 <style>
@@ -25,8 +30,19 @@
 </style>
 
 <h1>Familias</h1>
-<ul>
-  {#each familias as familia}
-    <li> {familia.nombreOriginario} </li>
-  {/each}
-</ul>
+{#await $familias}
+  Loading...
+{:then result}
+  {#if result.data}
+      {#each result.data.FamiliaMany as familia}
+        <li>{familia.nombreOriginario}</li>
+      {/each}
+  {:else}
+    Loading...
+  {/if}
+  <!-- {#each result.data.FamiliaMany as familia}
+    {familia.nombreOriginario}
+  {/each} -->
+{:catch error}
+  Error: {error}
+{/await}
